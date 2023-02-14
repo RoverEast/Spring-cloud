131613466c8ade5fe56c4c43b3fa786c5c46d159
Dzmitry_Charapianka <karandashDG@gmail.com>
2023-02-14 13:27:02 +0100

    + enable feign + enable eureka + change config path

--- organization-service/src/main/java/com/epam/organization/service/AccountServiceImpl.java
+++ organization-service/src/main/java/com/epam/organization/service/AccountServiceImpl.java
@@ -1,6 +1,9 @@
 package com.epam.organization.service;
 
+import com.epam.organization.client.AccountClient;
+import com.epam.organization.client.AccountClientImpl;
 import com.epam.organization.client.feign.AccountClientFeign;
+import com.epam.organization.client.loadbalnced.LoadBalancedAccountClientImpl;
 import com.epam.organization.dto.AccountDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,21 +15,13 @@ import org.springframework.stereotype.Service;
 public class AccountServiceImpl implements AccountService {
 
 
+//        private final AccountClientImpl accountClient;
+//        private final LoadBalancedAccountClientImpl accountClient;
     private final AccountClientFeign accountClient;
 
     @Override
-//    @CircuitBreaker(name = "accountService", fallbackMethod = "accountFallback")
     public AccountDto getAccountForOrganization(String organizationId) {
         return accountClient.getAccountByOrganizationId(organizationId);
-    }
-
-    public AccountDto accountFallback(String organizationId, Throwable t){
-        log.warn("Fallback triggered due to {}", t.getMessage());
-        return AccountDto.builder()
-                .accountNumber("0000000-00000-0000")
-                .currency("USD")
-                .isActive(true)
-                .build();
     }
 
 }
