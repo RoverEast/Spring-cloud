9fea5e4baece0762e7c86cc4c1c110ba522b143e
Dzmitry_Charapianka <karandashDG@gmail.com>
2023-02-20 12:26:11 +0100

    Added circuit breaker

--- organization-service/src/main/java/com/epam/organization/service/AccountServiceImpl.java
+++ organization-service/src/main/java/com/epam/organization/service/AccountServiceImpl.java
@@ -1,10 +1,8 @@
 package com.epam.organization.service;
 
-import com.epam.organization.client.AccountClient;
-import com.epam.organization.client.AccountClientImpl;
 import com.epam.organization.client.feign.AccountClientFeign;
-import com.epam.organization.client.loadbalnced.LoadBalancedAccountClientImpl;
 import com.epam.organization.dto.AccountDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,13 +13,21 @@ import org.springframework.stereotype.Service;
 public class AccountServiceImpl implements AccountService {
 
 
-//        private final AccountClientImpl accountClient;
-//        private final LoadBalancedAccountClientImpl accountClient;
     private final AccountClientFeign accountClient;
 
     @Override
+    @CircuitBreaker(name = "accountService", fallbackMethod = "accountFallback")
     public AccountDto getAccountForOrganization(String organizationId) {
         return accountClient.getAccountByOrganizationId(organizationId);
+    }
+
+    public AccountDto accountFallback(String organizationId, Throwable t){
+        log.warn("Fallback triggered due to {}", t.getMessage());
+        return AccountDto.builder()
+                .accountNumber("0000000-00000-0000")
+                .currency("USD")
+                .isActive(true)
+                .build();
     }
 
 }
